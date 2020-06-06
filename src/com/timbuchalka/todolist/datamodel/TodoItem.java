d3bd7d085cd92431a5e60c6f513edc5ad8d1cac7
AyoubRAHOUI <ayrahoui@gmail.com>
2020-06-06 23:13:37 +0200

    Add change Listener

--- src/com/timbuchalka/todolist/datamodel/TodoItem.java
+++ src/com/timbuchalka/todolist/datamodel/TodoItem.java
@@ -2,9 +2,6 @@ package com.timbuchalka.todolist.datamodel;
 
 import java.time.LocalDate;
 
-/**
- * Created by timbuchalka on 17/04/2016.
- */
 public class TodoItem {
 
     private String shortDescription;
